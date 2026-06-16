cbbffe586b2960ac6566e7d15456f73b21fa7558
PedroTiago23 <pedrotbiffi@gmail.com>
2026-06-16 17:06:23 -0300

    Implementação da concatenação

--- Index/DeleteBtree.h
+++ Index/DeleteBtree.h
@@ -11,4 +11,6 @@ int removerRecursivo(int chave);
 
 void reorganizarPagina(NO_ARVOREB* pagina);
 
+void concatenacaoArvoreB(NO_ARVOREB* pai, int posPai, NO_ARVOREB* esq, NO_ARVOREB* dir, int dirRRN, FILE* arqIndice, CABECALHO_ARVOREB* cabecalho);
+
 #endif
