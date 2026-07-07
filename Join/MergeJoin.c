2b35647e50a79fb6ae6f4cd42b68a07df323d4ad
Pedro Tiago Biffi <pedrotbiffi@gmail.com>
2026-07-07 17:57:52 -0300

    novas funcionalidades

--- Join/MergeJoin.c
+++ Join/MergeJoin.c
@@ -4,8 +4,205 @@ Bruno Dias de Campos Filho - 16832658
 Pedro Tiago Biffi - 16827777
 */
 
-void MERGE_JOIN()
-{
+#include "JoinHeaders.h"
+
+// Função usada pelo qsort para comparar a próxima estação
+int compara_merge_codProxEst(const void *a, const void *b) {
+    REGISTRO *r1 = a;
+    REGISTRO *r2 = b;
+
+    // Se os registros tiverem campos nulos
+    // serão considerados iguais
+    if (r1->codProxEstacao == -1 && r2->codProxEstacao == -1) {
+        return 0;
+    }
+
+    // retorna 1 quando o codproxEstacao do arquivo 1 é nulo
+    if (r1->codProxEstacao == -1) {
+        return 1;
+    }
+    
+    // retorna -1 quando o codproxEstacao do arquivo 2 é nulo
+    if (r2->codProxEstacao == -1) {
+        return -1;
+    }
+
+    // Caso ambos os codProxEstacao sejam válidos, subtraimos eles 
+    // Com base na implementação do qsort:
+    // Se o resultado for negativo, o qsort vai ordenar o r1 antes do r2
+    // Se for positivo, o r2 vem antes do r1
+    // Se der 0, vão ser equivalentes
+    return r1->codProxEstacao - r2->codProxEstacao;
+}
+
+// Função usada pelo qsort para comparar a estação atual
+int compara_merge_codEst(const void *a,const void *b) {
+    REGISTRO *r1 = a;
+    REGISTRO *r2 = b;
+
+    // Mesma lógica da função anterior, mas para o codEstacao
+    if (r1->codEstacao == -1 && r2->codEstacao == -1) {
+        return 0;
+    }
+
+    if (r1->codEstacao == -1) {
+        return 1;
+    }
+    
+    if (r2->codEstacao == -1) {
+        return -1;
+    }
+
+    return r1->codEstacao - r2->codEstacao;
+}
+
+void MERGE_JOIN() {
+    char nomeArq1[32];
+    char nomeCampo1[32];
+    char nomeArq2[32];
+    char nomeCampo2[32];
+
+    // Lê as entradas do usuário no terminal
+    scanf("%s %s %s %s", nomeArq1, nomeCampo1, nomeArq2, nomeCampo2);
+
+    // Verifica os campos que foram digitados
+    if (strncmp(nomeCampo1, "codProxEst", 10) != 0 || strcmp(nomeCampo2, "codEstacao") != 0) {
+        printf("Falha no processamento do arquivo.\n");
+        return;
+    }
+
+    // define e abre os arquivos no modo leitura binária 
+    FILE* arq1 = fopen(nomeArq1, "rb");
+    FILE* arq2 = fopen(nomeArq2, "rb");
+
+    // Verifica se os arquivos tiveram problemas para serem abertos 
+    if (arq1 == NULL || arq2 == NULL) {
+        printf("Falha no processamento do arquivo.\n");
+        
+        if (arq1 != NULL) {
+            fclose(arq1);
+        }
+        
+        if (arq2 != NULL) {
+            fclose(arq2);
+        }
+        
+        return;
+    }
+
+    // Define e le os cabeçalhos dos dois arquivos 
+    CABECALHO cab1;
+    CABECALHO cab2;
+    lerCabecalhoBin(arq1, &cab1);
+    lerCabecalhoBin(arq2, &cab2);
+
+    // Verifica se os arquivos não estão marcados como inconsistentes antes de mexer
+    if (cab1.status == '0' || cab2.status == '0') {
+        printf("Falha no processamento do arquivo.\n");
+        fclose(arq1);
+        fclose(arq2);
+        return;
+    }
+
+    // define o vetor1 para o arquivo1 e carrega na memória o vetor 
+    // de registros lidos que não são logicamente removidos ou nulos
+    REGISTRO *vetor1 = malloc(cab1.proxRRN * sizeof(REGISTRO));
+    // quantidade de registros validos que foram lidos
+    int qtd1 = 0;
+    
+    for (int i = 0; i < cab1.proxRRN; i++) {
+        REGISTRO reg;
+        LerRegistroBin(arq1, &reg, i);
+        
+        // Ignoramos lixo: só guardamos registros não removidos e com o campo válido
+        if (reg.removido == '0' && reg.codProxEstacao != -1) {
+            vetor1[qtd1] = reg;
+            qtd1++;
+        } else {
+            // se o registro for nulo ou removido, apenas libera a memoria dele 
+            // e não entra no vetor
+            liberaStringsRegistro(&reg);
+        }
+    }
+    fclose(arq1);
+
+    // Mesma lógica do vetor1, mas agora para o arquivo2
+    REGISTRO *vetor2 = malloc(cab2.proxRRN * sizeof(REGISTRO));
+    int qtd2 = 0;
+    
+    for (int i = 0; i < cab2.proxRRN; i++) {
+        REGISTRO reg;
+        LerRegistroBin(arq2, &reg, i);
+        
+        if (reg.removido == '0' && reg.codEstacao != -1) { 
+            vetor2[qtd2] = reg;
+            qtd2++;
+        } else {
+            liberaStringsRegistro(&reg);
+        }
+    }
+    fclose(arq2);
+
+    // Ordena ambos os vetores, o arquivo1 é ordenado pelo codProxEstacao
+    // o arquivo2 é ordenado pelo codEstacao
+    qsort(vetor1, qtd1, sizeof(REGISTRO), compara_merge_codProxEst);
+    qsort(vetor2, qtd2, sizeof(REGISTRO), compara_merge_codEst);
+
+    // Definindo e inicializando as variaveis que representam os ponteiros 
+    // dos arquivos 1 e 2, além da flag existe_um, que verifica se 
+    // o registro foi encontrado ou não existe
+    int p1 = 0;
+    int p2 = 0;
+    bool existe_um = false;
+
+    // Loop que busca nos dois vetores uma única vez. 
+    // Se algum dos dois vetores acabar, acaba o loop
+    while (p1 < qtd1 && p2 < qtd2) {
+        
+        if (vetor1[p1].codProxEstacao == vetor2[p2].codEstacao) {
+            // Encontrou o par que corresponde 
+            // imprime o resultado
+            printf("%d %s %s %d %s\n", vetor1[p1].codEstacao, vetor1[p1].nomeEstacao, vetor1[p1].nomeLinha, vetor2[p2].codEstacao, vetor2[p2].nomeEstacao);
+            existe_um = true;
+            
+            // Avança o ponteiro do vetor 1
+            // Já que o codEstacao do vetor 2 é único, o vetor 1 não vai achar mais ninguém igual a ele
+            p1++; 
+            
+
+          // Como ambos os arquivos estã ordenados em ordem crescente
+          // o arquivo1 em ordem crescente de codProxEstacao e o arquivo2
+          // em ordem crescente de codEstacao, pode acontecer de um ponteiro 
+          // de um arquivo estar na frente do outro arquivo
+
+          // Caso o ponteiro do arquivo1 está atrás do arquivo2
+        } else if (vetor1[p1].codProxEstacao < vetor2[p2].codEstacao) {
+            // Avança o ponteiro do arquivo1 
+            p1++;
+           
+          // Caso o ponteiro do arquivo2 esteja atrás do arquivo1 
+        } else {
+            // Avança o ponteiro do arquivo2
+            p2++;
+        }
+    }
+
+    // Caso não seja encontrado o par, a flag permanecerá como falsa.
+    if (existe_um == false) {
+        printf("Registro inexistente.\n");
+    }
+
+    
+    // Libera a memória de todos os campos necessários
+    for (int i = 0; i < qtd1; i++) {
+        liberaStringsRegistro(&vetor1[i]);
+    }
+    free(vetor1);
+    
+    for (int i = 0; i < qtd2; i++) {
+        liberaStringsRegistro(&vetor2[i]);
+    }
+    free(vetor2);
 
     return;
 }
--- Join/OrderBy.c
+++ Join/OrderBy.c
@@ -4,8 +4,147 @@ Bruno Dias de Campos Filho - 16832658
 Pedro Tiago Biffi - 16827777
 */
 
+#include "JoinHeaders.h"
+
+// Função de comparação para o qsort, relacionada ao codigo Estação
+int compara_codEstacao(const void *a, const void *b) {
+    REGISTRO *r1 = a;
+    REGISTRO *r2 = b;
+    
+    // nulos aparecem depois dos valores não nulos
+    if (r1->codEstacao == -1 && r2->codEstacao == -1) {
+        return 0;
+    }
+    
+    if (r1->codEstacao == -1) {
+        return 1;
+    }
+    
+    if (r2->codEstacao == -1) {
+        return -1;
+    }
+    
+    return r1->codEstacao - r2->codEstacao;
+}
+
+// Função de comparação para o qsort, mas agora relacionada ao codigo proxima Estação
+int compara_codProxEstacao(const void *a, const void *b) {
+    REGISTRO *r1 = a;
+    REGISTRO *r2 = b;
+    
+    // Mesma lógica da função compara_codEstacao
+    if (r1->codProxEstacao == -1 && r2->codProxEstacao == -1) {
+        return 0;
+    }
+    
+    if (r1->codProxEstacao == -1) {
+        return 1;
+    }
+    
+    if (r2->codProxEstacao == -1) {
+        return -1;
+    }
+    
+    return r1->codProxEstacao - r2->codProxEstacao;
+}
+
 void ORDER_BY()
 {
+    char arqEntradaNome[32];
+    char campoOrd[32];
+    char arqSaidaNome[32];
+    
+    scanf("%s %s %s", arqEntradaNome, campoOrd, arqSaidaNome);
 
+    // Verifica os campos de ordenação 
+    if (strcmp(campoOrd, "codEstacao") != 0 && strncmp(campoOrd, "codProxEst", 10) != 0) {
+        printf("Falha no processamento do arquivo.\n");
+        return;
+    }
+
+    // Abre e verifica o arquivo de entrada que é o arquivo de dados
+    FILE *arqEntrada = fopen(arqEntradaNome, "rb");
+    if (arqEntrada == NULL) {
+        printf("Falha no processamento do arquivo.\n");
+        return;
+    }
+
+    // define e lê o cabeçalho do arquivo de dados e verifica seu status 
+    // antes de fazer qualquer coisa
+    CABECALHO cab;
+    lerCabecalhoBin(arqEntrada, &cab);
+    
+    if (cab.status == '0') {
+        printf("Falha no processamento do arquivo.\n");
+        fclose(arqEntrada);
+        return;
+    }
+
+    // Define o array de ordenação e joga os valores lidos do disco para a RAM
+    // Só são válidos registros que não foram logicamente removidos
+    REGISTRO *vetorOrdenacao = malloc(cab.proxRRN * sizeof(REGISTRO));
+    int qtdValidos = 0;
+    
+    for (int i = 0; i < cab.proxRRN; i++) {
+        REGISTRO reg;
+        LerRegistroBin(arqEntrada, &reg, i);
+        
+        if (reg.removido == '0') {
+            vetorOrdenacao[qtdValidos] = reg; 
+            qtdValidos++;
+        } else {
+            // Se removido, limpamos memória das strings
+            liberaStringsRegistro(&reg); 
+        }
+    }
+    fclose(arqEntrada);
+
+    // ordenando conforme o campo escolhido usando o quicksort da biblioteca stdlib
+    // o quicksort usa as funções de comparação para saber qual é maior, menor ou nulo para ordenar
+    if (strcmp(campoOrd, "codEstacao") == 0) {
+        qsort(vetorOrdenacao, qtdValidos, sizeof(REGISTRO), compara_codEstacao);
+    } else {
+        qsort(vetorOrdenacao, qtdValidos, sizeof(REGISTRO), compara_codProxEstacao);
+    }
+
+    // Escrevendo o vetor ordenado no arquivo de saida(novo arquivo binário)
+    FILE *arqSaida = fopen(arqSaidaNome, "wb");
+    if (arqSaida == NULL) {
+        printf("Falha no processamento do arquivo.\n");
+        
+        for(int i = 0; i < qtdValidos; i++) {
+            liberaStringsRegistro(&vetorOrdenacao[i]);
+        }
+        free(vetorOrdenacao);
+        
+        return;
+    }
+
+    // Atualizando o cabecalho do novo arquivo de saida
+    CABECALHO cabSaida;
+    cabSaida.status = '0';
+    cabSaida.topo = -1;
+    cabSaida.proxRRN = qtdValidos; // No novo arquivo, proxRRN será a contagem contínua de válidos
+    cabSaida.nroEstacoes = cab.nroEstacoes;
+    cabSaida.nroParesEstacoes = cab.nroParesEstacoes;
+    
+    atualizarCabecalho(&cabSaida, arqSaida);
+
+    // Escreve no arquivo de saida o vetor ordenado e libera as strings e o vetor depois 
+    for (int i = 0; i < qtdValidos; i++) {
+        EscreverRegistroBin(arqSaida, &vetorOrdenacao[i]);
+        // Libera as strings após transferir para o disco
+        liberaStringsRegistro(&vetorOrdenacao[i]); 
+    }
+    free(vetorOrdenacao);
+
+    // Terminado todo o processo, atualiza o cabeçalho do arquivo de saida para consistente 
+    // Fecha o arquivo e imprime o binário na tela
+    cabSaida.status = '1';
+    atualizarCabecalho(&cabSaida, arqSaida);
+    fclose(arqSaida);
+
+    BinarioNaTela(arqSaidaNome);
+    
     return;
 }
